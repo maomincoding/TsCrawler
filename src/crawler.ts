7f5d955f371a771a9ac0a11ea042dd918638fd49
maomincoding <17864296568@163.com>
2021-01-10 02:23:33 +0800

    2021.1.10

--- src/crawler.ts
+++ src/crawler.ts
@@ -1,1 +1,16 @@
-console.log('111')
+import superagent from 'superagent'
+
+class Crowller {
+  private url = "http://www.kp980.com/";
+  private rawHtml = "";
+  async getRawHtml() {
+    const result = await superagent.get(this.url);
+    this.rawHtml = result.text;
+    console.log(result.text);
+  }
+  constructor() {
+    this.getRawHtml();
+  }
+}
+
+const crowller = new Crowller();
